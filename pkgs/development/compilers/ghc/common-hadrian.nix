3b5be1f840444e64688b3ad8844dec841bf2523d
ners <ners@gmx.ch>
2026-05-16 15:17:50 +0200

    haskell.compiler.ghcZigHEAD: fix Hadrian flags some more

--- pkgs/development/compilers/ghc/common-hadrian.nix
+++ pkgs/development/compilers/ghc/common-hadrian.nix
@@ -408,7 +408,6 @@ let
       "*.*.ghc.c.opts += -optc--target=${stdenv.system}-gnu-2.27"
       "*.*.ghc.link.opts += -optl--target=${stdenv.system}-gnu-2.27"
       "*.*.cc.c.opts += --target=${stdenv.system}-gnu-2.27"
-      "*.*.cc.link.opts += --target=${stdenv.system}-gnu-2.27"
     ]
     # -fexternal-dynamic-refs apparently (because it's not clear from the
     # documentation) makes the GHC RTS able to load static libraries, which may
